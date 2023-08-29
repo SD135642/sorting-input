dc2d0dbd2e5ef7836d092c6b32ca1c7bbff7b37a
SD135642 <123038434+SD135642@users.noreply.github.com>
2023-08-29 12:09:40 -0400

    Update quicksort.h

--- include/quicksort.h
+++ include/quicksort.h
@@ -1,1 +1,14 @@
-//?
+static void swap(void *a, void *b, size_t size);
+static int lomuto(void *array, int left, int right, size_t elem_sz,
+                    int (*cmp)(const void*, const void*));
+static void quicksort_helper(void *array, int left, int right, size_t elem_sz,
+                                int (*cmp)(const void*, const void*));
+
+int int_cmp(const void *a, const void *b);
+
+int dbl_cmp(const void *a, const void *b);
+
+int str_cmp(const void *a, const void *b);
+
+void quicksort(void *array, size_t len, size_t elem_sz, int (*cmp)(const void*, const void*));
+
